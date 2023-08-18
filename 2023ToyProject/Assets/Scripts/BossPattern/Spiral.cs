61f03ee51c3596f64c2bab33175e452c603f90af
r01ex <102461975+r01ex@users.noreply.github.com>
2023-08-19 03:28:01 +0900

    helix
    pattern added

--- 2023ToyProject/Assets/Scripts/BossPattern/Spiral.cs
+++ 2023ToyProject/Assets/Scripts/BossPattern/Spiral.cs
@@ -17,7 +17,7 @@ public class Spiral : MonoBehaviour
     }
     public void debug()
     {
-        StartCoroutine(Shoot(6, 200, 30, 20));
+        StartCoroutine(Shoot(6, 20, 30, 20));
     }
     public IEnumerator Shoot(float moveSpeed, int totalbullet, int interval, float angle_increment)
     {
