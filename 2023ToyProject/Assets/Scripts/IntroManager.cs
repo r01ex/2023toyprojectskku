bfcc20a623c63ab4160bd486f01bf60686e6d0d1
0-0325 <ryan160060@gmail.com>
2023-08-22 03:00:06 +0900

    Add Attack to HCl
    .

--- 2023ToyProject/Assets/Scripts/IntroManager.cs
+++ 2023ToyProject/Assets/Scripts/IntroManager.cs
@@ -8,10 +8,19 @@ public class IntroManager : MonoBehaviour
     ///To contain times during several Panels to transition animation
     [SerializeField]
     private float transitionTime;
+    [SerializeField]
+    private AudioClip audioLogo;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
     void Start()
     {
+        audioSource.clip = audioLogo;
+        audioSource.Play();
         StartCoroutine(DelayTime(transitionTime));
     }
 
